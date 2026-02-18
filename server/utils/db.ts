57316114e0d54017c37186f07e93f84e97ce0526
BlackRNNT <blackrnnt@gmail.com>
2026-02-18 17:09:41 +0200

    fix: Prisma db path envirment

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,16 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const dbPath = path.join(__dirname, '../../prisma/dev.db')
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: `file:${dbPath}`,
-    },
-  },
-})
+const prisma = new PrismaClient()
 
 export default prisma
